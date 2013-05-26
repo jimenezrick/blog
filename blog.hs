90b710d55bb86c793af9f69216677d6d9fce9506
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2013-05-26 23:22:04 +0200

    Add render from Markdown to HTML with Pandoc

--- blog.hs
+++ blog.hs
@@ -2,10 +2,12 @@
 
 import Control.Monad
 import Control.Monad.Reader
+import System.FilePath ((</>))
 
 import qualified Data.Text.Lazy                as T
 import qualified System.Directory              as D
 
+import qualified Text.Pandoc                   as P
 import qualified Web.Scotty                    as S
 import qualified Text.Blaze.Html5              as H
 import qualified Text.Blaze.Html.Renderer.Text as R
@@ -16,7 +18,7 @@ data Config = Config { port  :: Int
                      } deriving (Show, Read)
 
 defaultConfig :: Config
-defaultConfig = Config 8000 "/" "My Blog"
+defaultConfig = Config 8000 "/tmp" "My Blog"
 
 main :: IO ()
 main = do
@@ -31,14 +33,18 @@ main = do
 renderIndex :: Blog H.Html
 renderIndex = do
     title <- blogTitle
-    posts <- liftM (map H.toHtml) listPosts
+    index <- liftM (map H.toHtml) listPosts
+    post <- renderPost "post.md" -- XXX
     return $ H.docTypeHtml $ do
         H.head $ do
             H.title $ H.toHtml title
         H.body $ do
-            H.h1 "rlog"
+            H.h1 "Blog"
             H.ul $ do
-                mapM_ H.li posts
+                mapM_ H.li index
+            H.hr -- XXX
+            post
+
 
 
 
@@ -56,3 +62,15 @@ httpPort = liftM port ask
 
 blogTitle :: Blog T.Text
 blogTitle = liftM title ask
+
+
+
+
+
+
+renderPost :: FilePath -> Blog H.Html
+renderPost path = do
+    root <- postsRoot
+    text <- liftIO $ readFile $ root </> path
+    -- TODO: try readMarkdownWithWarnings
+    return $ P.writeHtml P.def $ P.readMarkdown P.def text
