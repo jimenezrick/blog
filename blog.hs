5c871b677684f1216b38584c2d8b1112c78eed56
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2013-06-06 21:43:27 +0200

    Add customizable post extension and parser

--- blog.hs
+++ blog.hs
@@ -19,11 +19,13 @@ import qualified Web.Scotty                  as S
 import qualified Text.Blaze.Html5            as H
 import qualified Text.Blaze.Html5.Attributes as HA
 
-data Config = Config { configPort  :: Int
-                     , configRoot  :: FilePath
-                     , configTitle :: Text -- XXX: Configure title/intro in another place?
-                     , configCss   :: FilePath
-                     } deriving (Show, Read)
+data Config = Config { configPort       :: Int
+                     , configRoot       :: FilePath
+                     , configCss        :: FilePath
+                     , configTitle      :: Text -- XXX: Configure title/intro in another place?
+                     , configPostExt    :: String
+                     , configPostParser :: PostParser
+                     }
 
 -- XXX: Configure Warp
 -- XXX: Add wai-static handler for serving statics
@@ -31,23 +33,26 @@ data Config = Config { configPort  :: Int
 -- XXX: Show more info about a post, date? Add home link in each post
 -- XXX: look in my web/r-log hakyll previous experiment
 defaultConfig :: Config
-defaultConfig = Config 8000 "posts" "rlog" "/static/style.css"
+defaultConfig = Config 8000 "posts" "rlog" "/static/style.css" ".md" P.readMarkdown
 
 type Blog = ReaderT Config IO
+
+type PostParser = P.ReaderOptions -> String -> Pandoc
 
 main :: IO ()
 main = do
     -- XXX: Handle exceptions from here
-    S.scotty (configPort defaultConfig) $ do
+    S.scotty (configPort conf) $ do
         S.get "/blog" $ do
             index <- dispatch renderIndex
             render index
         S.get (S.regex "^/post/(.+)$") $ do
             path <- S.param "1"
-            post <- dispatch $ renderPost (path ++ ".md")
+            post <- dispatch $ renderPost (path ++ configPostExt conf)
             render post
-    where dispatch = liftIO . flip runReaderT defaultConfig
+    where dispatch = liftIO . flip runReaderT conf
           render   = S.html . renderHtml
+          conf     = defaultConfig
 
 renderHead :: FilePath -> Text -> H.Html
 renderHead path title =
@@ -89,8 +94,8 @@ postLink = link . ("post" </>) . dropExtension
 renderPostName :: FilePath -> Blog H.Html
 renderPostName path = do
     text <- readPost path
-    let post  = fromMarkdown text
-        title = postTitle path post
+    post <- parsePost text
+    let title = postTitle path post
     return $ H.toHtml title
 
 postTitle :: FilePath -> Pandoc -> String
@@ -111,8 +116,8 @@ renderPost :: FilePath -> Blog H.Html
 renderPost path = do
     cssPath <- reader configCss
     text    <- readPost path
-    let post    = fromMarkdown text
-        content = toHtml post
+    post    <- parsePost text
+    let content = toHtml post
         title   = pack $ postTitle path post
     return $ H.docTypeHtml $ do
         renderHead cssPath title
@@ -128,27 +133,27 @@ readPost path = do
     text <- liftIO $ readFile $ root </> path
     return text
 
-fromMarkdown :: String -> Pandoc
-fromMarkdown = P.readMarkdown P.def
+parsePost :: String -> Blog Pandoc
+parsePost text = do
+    parser <- reader configPostParser
+    return (parser P.def text)
 
 toHtml :: Pandoc -> H.Html
 toHtml = P.writeHtml P.def {writerHtml5 = True}
 
-isMarkdown :: FilePath -> Bool
-isMarkdown = (".md" ==) . takeExtension
-
 searchPosts :: Blog [FilePath]
 searchPosts = do
     root <- reader configRoot
-    liftIO $ search root "."
-        where search root parent = do
+    ext  <- reader configPostExt
+    liftIO $ search ext root "."
+        where search ext root parent = do
                   files <- listDir (root </> parent)
                   paths <- forM files $ \file -> do
                       let path = parent </> file
                       isDir <- doesDirectoryExist (root </> path)
                       if isDir
-                        then search root path
-                        else return $ do guard (isMarkdown file)
+                        then search ext root path
+                        else return $ do guard (takeExtension file == ext)
                                          [path]
                   return (concat paths)
 
